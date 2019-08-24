590f6ec42798f7ce599b70429a2845256652b7e7
Samuel Lippl <sfc.lippl@gmail.com>
2019-08-24 16:33:32 +0100

    Change name to lazytools_sflippl

---
+++ lazytools_sflippl/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 12:35:02 2019
+
+@author: sflippl
+"""
+
+name = 'lazytools_sflippl'
+
+from lazytools_sflippl.spyder import *
+from lazytools_sflippl.exceptions import *
+from lazytools_sflippl.to_dataframe import *
+from lazytools_sflippl.visualizations import *
