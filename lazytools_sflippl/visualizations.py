b5e2fae2b1db879ec96307edd912df31fae74133
Samuel Lippl <sfc.lippl@gmail.com>
2019-08-27 16:44:42 +0100

    Correct legend title.

--- lazytools_sflippl/visualizations.py
+++ lazytools_sflippl/visualizations.py
@@ -3,18 +3,22 @@ import plotnine as gg
 
 from lazytools_sflippl.to_dataframe import *
 
-def matrix_heatmap(matrix, pole = None):
-    """
-    Returns a matrix heatmap, either centered around a pole or unidirectional
-    (if pole is 'None', the default option).
+def matrix_heatmap(matrix, pole=None):
+    """Get a matrix heatmap.
+
+    Args:
+        matrix: Two dimensional numpy array.
+    Returns:
+        A matrix heatmap, either centered around a pole or unidirectional
+        (if pole is 'None', the default option).
     """
     assert matrix.ndim == 2
     df_matrix = array_to_dataframe(matrix)
-    heatmap = (gg.ggplot(df_matrix, gg.aes(x = 'dim1', y = 'dim0', fill = 'array')) + 
+    heatmap = (gg.ggplot(df_matrix, gg.aes(x='dim1', y='dim0', fill='array')) + 
               gg.geom_tile() + 
               gg.theme_void() + 
               gg.scale_y_reverse() + 
-              gg.labs(fill = None))
+              gg.theme(legend_title = gg.element_blank()))
     if pole is None:
         heatmap += gg.scale_fill_gradient(low = 'white', high = 'black')
     else:
